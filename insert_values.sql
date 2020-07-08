35d9903398db7aaecf4ab1c619da5a87121d1e6b
E-Adluru <67987171+E-Adluru@users.noreply.github.com>
2020-07-07 20:23:25 -0700

    Delete insert_values.sql

--- insert_values.sql
+++
@@ -1,7 +0,0 @@
-insert into cart values('crt1011');
-insert into customer values('cid100','rajat','G-453','632014',9893135876, 'crt1011');
-insert into sellers values('sid100','12345','aman','delhi cmc');
-insert into products values('pid1001','jeans','red','32','M',10,10005,20,'sid100');
-insert into seller_contact values('9943336206','sid100');
-insert into items_in_cart values(3,'1999-10-10','crt1011','pid1001','Y');
-insert into payments values('pmt1001','1999-10-10','online','cid100','crt1011',NULL);
